d878de90e175c7d0328e21f4b051394dcee19fff
NobiteK <81562447+NobiteK@users.noreply.github.com>
2023-07-02 14:18:21 +0200

    Update PreloaderJS.js

--- assets/Js/PreloaderJS.js
+++ assets/Js/PreloaderJS.js
@@ -1,3 +1,30 @@
-setTimeout(function() {
-   $("#preloader").fadeOut("slow");
-}, 2500);
+// Hide preloader after 2 seconds
+
+   setTimeout(function() {
+      $("#preloader").fadeOut("slow");
+   }, 2000);
+   
+// Disable scrolling and hide scrollbar for 2.6 seconds
+   
+   var loader = document.getElementById("preloader");
+   window.addEventListener("load", function() {
+     window.addEventListener("scroll", disableScroll);
+     hideScrollbar();
+   
+     setTimeout(function() {
+       window.removeEventListener("scroll", disableScroll);
+       showScrollbar();
+     }, 2600);
+   });
+   
+   function disableScroll() {
+     window.scrollTo(0, 0);
+   }
+   
+   function hideScrollbar() {
+     document.documentElement.style.overflow = 'hidden';
+   }
+   
+   function showScrollbar() {
+     document.documentElement.style.overflow = 'auto';
+   }
