b07af475a5775a2c9ff48856724bb3f9cb173a56
NobiteK <81562447+NobiteK@users.noreply.github.com>
2024-01-15 18:31:36 +0100

    Update antisteal.js
    Fixed deprecated functions.

--- assets/Js/antisteal.js
+++ assets/Js/antisteal.js
@@ -1,59 +1,55 @@
 window.onload = function () {
-    document.addEventListener("contextmenu", function (e) {
-      e.preventDefault();
-      showNotification("Nuh Uh");
-    }, false);
+  document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+    showNotification("Nuh Uh");
+  }, false);
 
-    document.addEventListener("keydown", function (e) {
-      //  Ctrl + Shift + I
-      if (e.ctrlKey && e.shiftKey && e.key == 73) {
-        disabledEvent(e);
-      }
-      // Ctrl + Shift + J
-      if (e.ctrlKey && e.shiftKey && e.key == 74) {
-        disabledEvent(e);
-      }
-      // Ctrl + Shift + C
-      if (e.ctrlKey && e.shiftKey && e.key == 67) {
-        disabledEvent(e);
-      }
-      // "S" key + macOS
-      if (e.key == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-        disabledEvent(e);
-      }
-      // Ctrl + U (Source)
-      if (e.ctrlKey && e.key == 85) {
-        disabledEvent(e);
-      }
-      // Ctrl + I (Mozilla Site Info)
-      if (e.ctrlKey && e.key == 73) {
-        disabledEvent(e);
-      }
-      // F12
-      if (e.key == 123) {
-        disabledEvent(e);
-      }
-    }, false);
-
-    function disabledEvent(e) {
-      if (e.stopPropagation) {
-        e.stopPropagation();
-      } else if (window.event) {
-        window.event.cancelBubble = true;
-      }
-      e.preventDefault();
-      showNotification("Nuh Uh");
-      return false;
+  document.addEventListener("keydown", function (e) {
+    //  Ctrl + Shift + I
+    if (e.ctrlKey && e.shiftKey && e.key === "I") {
+      disabledEvent(e);
     }
-
-    function showNotification(message) {
-      var notification = document.getElementById("notification");
-      notification.textContent = message;
-      notification.style.transform = "translateY(0%)";
-      notification.style.opacity = "1";
-      setTimeout(function () {
-        notification.style.transform = "translateY(100%)";
-        notification.style.opacity = "0";
-      }, 2000);
+    // Ctrl + Shift + J
+    if (e.ctrlKey && e.shiftKey && e.key === "J") {
+      disabledEvent(e);
     }
+    // Ctrl + Shift + C
+    if (e.ctrlKey && e.shiftKey && e.key === "C") {
+      disabledEvent(e);
+    }
+    // "S" key + macOS
+    if (e.key === "S" && (navigator.userAgent.includes("Mac") ? e.metaKey : e.ctrlKey)) {
+      disabledEvent(e);
+    }
+    // Ctrl + U (Source)
+    if (e.ctrlKey && e.key === "U") {
+      disabledEvent(e);
+    }
+    // Ctrl + I (Mozilla Site Info)
+    if (e.ctrlKey && e.key === "I") {
+      disabledEvent(e);
+    }
+    // F12
+    if (e.key === "F12") {
+      disabledEvent(e);
+    }
+  }, false);
+
+  function disabledEvent(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    showNotification("Nuh Uh");
+    return false;
   }
+
+  function showNotification(message) {
+    var notification = document.getElementById("notification");
+    notification.textContent = message;
+    notification.style.transform = "translateY(0%)";
+    notification.style.opacity = "1";
+    setTimeout(function () {
+      notification.style.transform = "translateY(100%)";
+      notification.style.opacity = "0";
+    }, 2000);
+  }
+}
