a143a3a88eab01d3603c68607cea51e8ad0673cb
NobiteK <81562447+NobiteK@users.noreply.github.com>
2024-04-14 23:33:07 +0200

    Update PreloaderJS.js

--- assets/Js/PreloaderJS.js
+++ assets/Js/PreloaderJS.js
@@ -1,33 +1,41 @@
-// Check if the user is coming from another page on the same website
-if (document.referrer && document.referrer.indexOf(window.location.hostname) !== -1) {
-  // If so, hide the preloader immediately
-  $("#preloader").hide();
-} else {
-  // If not, show the preloader for 2 seconds before fading out
+$(document).ready(function() {
+  // Hide preloader after 2 seconds
   setTimeout(function() {
     $("#preloader").fadeOut("slow");
   }, 2000);
-}
 
-// Disable scrolling and hide scrollbar for 2.6 seconds
-var loader = document.getElementById("preloader");
-window.addEventListener("load", function() {
-  window.addEventListener("scroll", disableScroll);
-  hideScrollbar();
-  setTimeout(function() {
-    window.removeEventListener("scroll", disableScroll);
-    showScrollbar();
-  }, 2600);
+  // Disable scrolling and hide scrollbar for 2.6 seconds
+  var loader = document.getElementById("preloader");
+  window.addEventListener("load", function() {
+    window.addEventListener("scroll", disableScroll);
+    hideScrollbar();
+    setTimeout(function() {
+      window.removeEventListener("scroll", disableScroll);
+      showScrollbar();
+    }, 2600);
+  });
+
+  function disableScroll() {
+    window.scrollTo(0, 0);
+  }
+
+  function hideScrollbar() {
+    document.documentElement.style.overflow = 'hidden';
+  }
+
+  function showScrollbar() {
+    document.documentElement.style.overflow = 'auto';
+  }
+
+  // Fade in content when switching from any other page on the website
+  $("body").css("display", "none");
+  $("body").fadeIn(1000);
+  
+  $("a").click(function(event) {
+    event.preventDefault();
+    var linkLocation = this.href;
+    $("body").fadeOut(1000, function() {
+      window.location = linkLocation;
+    });
+  });
 });
-
-function disableScroll() {
-  window.scrollTo(0, 0);
-}
-
-function hideScrollbar() {
-  document.documentElement.style.overflow = 'hidden';
-}
-
-function showScrollbar() {
-  document.documentElement.style.overflow = 'auto';
-}
