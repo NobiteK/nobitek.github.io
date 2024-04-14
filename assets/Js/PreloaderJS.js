4d51e96058935e6ab38cdbf789c899eb7d7520e8
NobiteK <81562447+NobiteK@users.noreply.github.com>
2024-04-14 23:39:49 +0200

    Update PreloaderJS.js

--- assets/Js/PreloaderJS.js
+++ assets/Js/PreloaderJS.js
@@ -1,36 +1,41 @@
-$(document).ready(function() {
-  // Hide preloader after 2 seconds
-  setTimeout(function() {
-    $("#preloader").fadeOut("slow");
-  }, 2000);
+// Hide preloader after 2 seconds
 
-  // Disable scrolling and hide scrollbar for 2.6 seconds
-  var loader = document.getElementById("preloader");
-  window.addEventListener("load", function() {
-    window.addEventListener("scroll", disableScroll);
-    hideScrollbar();
-    setTimeout(function() {
-      window.removeEventListener("scroll", disableScroll);
-      showScrollbar();
-    }, 2600);
-  });
+   setTimeout(function() {
+      $("#preloader").fadeOut("slow");
+   }, 2000);
+   
+// Disable scrolling and hide scrollbar for 2.6 seconds
+   
+   var loader = document.getElementById("preloader");
+   window.addEventListener("load", function() {
+     window.addEventListener("scroll", disableScroll);
+     hideScrollbar();
+   
+     setTimeout(function() {
+       window.removeEventListener("scroll", disableScroll);
+       showScrollbar();
+     }, 2600);
+   });
+   
+   function disableScroll() {
+     window.scrollTo(0, 0);
+   }
+   
+   function hideScrollbar() {
+     document.documentElement.style.overflow = 'hidden';
+   }
+   
+   function showScrollbar() {
+     document.documentElement.style.overflow = 'auto';
+   }
 
-  function disableScroll() {
-    window.scrollTo(0, 0);
+  // Check if user came from another site or directly entered the URL
+  if (!document.referrer || new URL(document.referrer).hostname !== window.location.hostname) {
+    $("body").css("display", "none");
+    $("body").fadeIn(1000);
   }
 
-  function hideScrollbar() {
-    document.documentElement.style.overflow = 'hidden';
-  }
-
-  function showScrollbar() {
-    document.documentElement.style.overflow = 'auto';
-  }
-
-  // Fade in content when switching from any other page on the website
-  $("body").css("display", "none");
-  $("body").fadeIn(1000);
-  
+  // Fade out and redirect when clicking on a link
   $("a").click(function(event) {
     event.preventDefault();
     var linkLocation = this.href;
