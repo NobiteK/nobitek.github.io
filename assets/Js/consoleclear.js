cb9e0d0b5e6f1aecf59156d4829fe0860ddc751d
NobiteK <81562447+NobiteK@users.noreply.github.com>
2024-04-19 00:59:11 +0200

    Delete assets/Js/consoleclear.js

--- assets/Js/consoleclear.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function() {
-  console.clear();
-  
-setTimeout(function() {
-    console.log('%c    Console Cleared!    ', 'padding: 15px; background-color: cyan; color: navy; font-weight: bold;');
-  }, 1000);
-}, 4000);
